fed95400a6ee8196b823ab4883f3888ed571e4f8
Park Jong Chan <73544708+JJong0416@users.noreply.github.com>
2021-08-05 12:10:37 +0900

    Solve 3256_210805_asura.py

---
+++ Auguest_/210807/2156_Taste_Wine/3256_210805_asura.py
@@ -0,0 +1,28 @@
+N = int(input())
+data = [0] * 10000
+for i in range(N):
+    data[i] = int(input())
+
+dp = [0] * 10000 # 전체 테이블 생성
+dp[0] = data[0]
+dp[1] = data[0] + data[1] # 1
+dp[2] = max(dp[1], data[2] + data[0], data[2] + data[1]) # 2
+
+for i in range(3,N): # 점화식
+    dp[i] = max(dp[i-1],data[i] + data[i-1] + dp[i-3], data[i] + dp[i-2])
+
+print(max(dp))
+
+
+"""
+1. 마시지 않거나,
+2. 지금꺼 마시고, 전전꺼 total 마시거나
+3. 지금꺼 마시고, 전에꺼 마시고, 전전전꺼 total 마시고
+
+인덱스 에러 이 부분에서 왜 뜨는 걸까요....
+data = []
+
+for _ in range(N):
+    data.append(int(input()))
+"""
+
