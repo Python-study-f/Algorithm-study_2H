ac56a8d9aa391e925f491376d2a365d600773cc3
Hahyun Lee <hahyuning@gmail.com>
2021-08-18 11:44:47 +0900

    Update 81302_210818_hahyuning.py

--- Auguest_/210821/81302_Keep_Distance/81302_210818_hahyuning.py
+++ Auguest_/210821/81302_Keep_Distance/81302_210818_hahyuning.py
@@ -41,4 +41,3 @@ def solution(places):
             answer.append(1)
     return answer
 
-print(solution([["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"], ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"], ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"], ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"], ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"]]))
