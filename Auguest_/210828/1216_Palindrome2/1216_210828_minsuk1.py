833a5dc1fba5a387356096a13548ca81e865b5dd
minsuk <palt1230@naver.com>
2021-08-28 17:51:34 +0900

    Solve 1216_210828_minsuk1.py

--- Auguest_/210828/1216_Palindrome2/1216_210828_minsuk1.py
+++ Auguest_/210828/1216_Palindrome2/1216_210828_minsuk1.py
@@ -12,8 +12,6 @@ for t in range(1, 11):
         if result >= k:
             break
         for i in range(100):
-            if result == k:
-                break
             for j in range(100-k+1):
                 if check(list1[i][j:j+k]) or check(list2[i][j:j+k]):
                     result=k
