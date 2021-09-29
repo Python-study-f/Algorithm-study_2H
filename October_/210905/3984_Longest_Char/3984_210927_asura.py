9fcb6ab4a532e0f8c64869d0190ce5ffef84b0bf
Park Jong Chan <73544708+JJong0416@users.noreply.github.com>
2021-09-29 23:39:32 +0900

    Update 3984_210927_asura.py

--- October_/210905/3984_Longest_Char/3984_210927_asura.py
+++ October_/210905/3984_Longest_Char/3984_210927_asura.py
@@ -1,13 +1,17 @@
 from itertools import combinations
 
-arr = ["abcdefghijklmnopqrstuvwxyz"]
-ans = 0
+class Solution(object):
+    def maxLength(self, arr):
+        ans = 0
 
-for s in range(len(arr)): # 조합이 2개가 아니라 3개,4개 여러개가 될 수 있기 때문에 combination(arr,2)로 하면 안된다. 접근 자체가 틀렸었음
-    for lst in combinations(arr,s+1):
-        check = ''.join(lst)
-        length = len(check)
+        for s in range(len(arr)): 
+            # 조합이 2개가 아니라 3개,4개 여러개가 될 수 있기 때문에 
+            #combination(arr,2)로 하면 안된다. 접근 자체가 틀렸었음
 
-        if length == len(set(check)):
-            ans = max(ans,length)
-print(ans)
+            for lst in combinations(arr,s+1):
+                check = ''.join(lst)
+                length = len(check)
+
+                if length == len(set(check)):
+                    ans = max(ans,length)
+        return ans
